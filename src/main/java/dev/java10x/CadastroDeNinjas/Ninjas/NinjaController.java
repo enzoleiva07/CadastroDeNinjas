d321db56e2388476862a29d5d93d363c96653f20
enzoleiva07 <enzoleiva654@gmail.com>
2025-09-23 23:37:53 -0400

    Modelagem do banco de dados feita - NinjaModel e MissoesModel concluídos. Anotations feitas para relacionamento de tabelas.

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/BOAS_VINDAS")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota!!";
+    }
+
+}
