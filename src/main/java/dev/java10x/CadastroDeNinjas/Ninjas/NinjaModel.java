1dcd795254f07626457d936bc1c0d4b5636f44c4
enzoleiva07 <enzoleiva654@gmail.com>
2025-09-27 19:14:30 -0400

    fix: NinjaRepository, and MissoesRepository adjusted to interfaces instead of classes, and adds @Column on NinjaModel class

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,14 +18,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "idade")
     private int idade;
 
     @Column(unique = true)
     private String email;
+
+    @Column(name = "img_url")
+    private String imgURL;
 
     // @ManyToOne - Um ninja está atribuido em apenas uma missão.
     @ManyToOne
